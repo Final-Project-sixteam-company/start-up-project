9207e0ad32b15e9b705b0d41695758b88a8cd718
sosugyeong <sugyeong247@gmail.com>
2026-05-19 17:20:25 +0900

    feat: 시나리오 enum 생성

---
+++ src/main/java/com/startup/domain/scenario/enums/ScenarioVisibility.java
@@ -0,0 +1,8 @@
+package com.startup.domain.scenario.enums;
+
+public enum ScenarioVisibility {
+    PRIVATE, //작성자만 접근 가능
+    UNLISTED, //링크가 있는 사용자만 접근 가능
+    PUBLIC, //모두 접근 가능
+    OFFICIAL //공식 시나리오, 모두 접근 가능
+}
